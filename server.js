d3098597baa771e25db2304b203a8101f987529c
rohwedernt <rohwedernt@gmail.com>
2019-04-13 20:23:53 -0400

    configure db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue'), ForbiddenError = epilogue.Errors.ForbiddenError;
 const app = express();
 
 // Serve the static files from the React app
@@ -22,3 +23,32 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+
+
+//Database setup
+const database = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite',
+    operatorsAliases: false,
+});
+
+const Post = database.define('posts', {
+    title: Sequelize.STRING,
+    img: Sequelize.STRING,
+    content: Sequelize.TEXT,
+});
+
+epilogue.initialize({ app, sequelize: database });
+
+const PostResource = epilogue.resource({
+    model: Post,
+    endpoints: ['/posts', '/posts/:id'],
+});
+
+PostResource.all.auth(function (req, res, context) {
+    return new Promise(function (resolve, reject) {
+        resolve(context.continue);
+    })
+});
+
+database.sync();
